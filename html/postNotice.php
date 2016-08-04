29e9558519a0c1f02807c73780b777e0775e412d
tennyuu921 <tmtenyu@gmail.com>
2016-08-04 17:28:24 +0900

    postNoticeの仕様をコメント

---
+++ html/postNotice.php
@@ -0,0 +1,55 @@
+<?php
+
+
+	//TODO 	DBへのINSERT機能の作成 
+	//		- バリーデーション
+	//		- DBへのINSERT処理
+	//			- 失敗した場合はエラーと表示
+
+
+		/* DB仕様
+
+		notice TABLE
+			id int,
+				=> NULL
+			title char,
+				=> タイトル(validation.空文字チェック)
+			comment text,
+				=> コメントを入力(validation.空文字チェック)
+			addfile char
+				=> NULL	
+			created date
+				=> 投稿が行われた時刻	
+			modified date
+				=> 投稿が行われた時刻
+		*/
+
+
+?>
+
+
+
+<!--        
+
+	TODO:お知らせ投稿用フォーム作成（ファイルアップロード除く）
+
+	フォーム
+		以下の２項目
+
+		1.title
+			=> タイトル(validation.空文字チェック,文字制限30文字)
+		2.comment
+			=> コメントを入力(validation.空文字チェック)
+			
+-->
+
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+
+</body>
+</html>
